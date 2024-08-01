56a8f1bbeb3cd9fb3e3faf3ff03f0985a81f23d6
saira-gulamali <sguacamole98@gmail.com>
2024-08-01 12:01:13 +0100

    add Big O for fibonacci

--- 02-recursion/16-fibonacci/script.js
+++ 02-recursion/16-fibonacci/script.js
@@ -1,11 +1,10 @@
-const fibonacciIterative = (n) => {
-  const arr = [0, 1];
-  // [0,1,1, 2,3,5]
-  for (let i = 2; i <= n; i++) {
-    arr.push(arr[i - 2] + arr[i - 1]);
-  }
-  return arr[n];
-};
+// Big O
+// Time: O(2^n)
+// solution forms a tree, as each node calls 2 more nodes/recursive calls
+// as the next call is n-1 this continues until we hit the base case of 0/1
+// so it continues to call fib twice n times
+// Space:O(n)
+// call stack max size is the tree depth (n) because nodes/bsae cases have to be popped off before other nodes can be added to the stack
 
 const fibonacci = (n) => {
   if (n === 0) {
@@ -18,16 +17,25 @@ const fibonacci = (n) => {
   return fibonacci(n - 2) + fibonacci(n - 1);
 };
 
-console.log(fibonacciIterative(0)); // -> 0
-console.log(fibonacciIterative(1)); // -> 1
-console.log(fibonacciIterative(2)); // -> 1
-console.log(fibonacciIterative(3)); // -> 2
-console.log(fibonacciIterative(4)); // -> 3
-console.log(fibonacciIterative(6)); // -> 5
-
 console.log(fibonacci(0)); // -> 0
 console.log(fibonacci(1)); // -> 1
 console.log(fibonacci(2)); // -> 1
 console.log(fibonacci(3)); // -> 2
 console.log(fibonacci(4)); // -> 3
 console.log(fibonacci(5)); // -> 5
+
+// iterative approach
+const fibonacciIterative = (n) => {
+  const arr = [0, 1];
+  // [0,1,1, 2,3,5]
+  for (let i = 2; i <= n; i++) {
+    arr.push(arr[i - 2] + arr[i - 1]);
+  }
+  return arr[n];
+};
+console.log(fibonacciIterative(0)); // -> 0
+console.log(fibonacciIterative(1)); // -> 1
+console.log(fibonacciIterative(2)); // -> 1
+console.log(fibonacciIterative(3)); // -> 2
+console.log(fibonacciIterative(4)); // -> 3
+console.log(fibonacciIterative(6)); // -> 5
